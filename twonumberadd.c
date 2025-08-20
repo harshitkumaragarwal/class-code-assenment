2c09746d77a6c8e11646a4a52442c5f32bfcb6bc
harshitkumaragarwal <agarwalharshit063@gmail.com>
2025-08-21 01:49:33 +0530

    name age assigment

---
+++ twonumberadd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    int sum;
+    printf("Enter a and b:");
+    scanf("%d %d",&a,&b);
+
+    sum=a+b;
+    
+    printf("sum of two numbers is:%d",sum);
+    return 0; 
+}
