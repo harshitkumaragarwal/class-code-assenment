8d392d50444ade7163e01c51d638de5314430466
harshitkumaragarwal <agarwalharshit063@gmail.com>
2025-09-08 21:49:41 +0530

    lab5 question 1

---
+++ uhbfus.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, k, n, num = 1;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        
+        for (k = 1; k <= n - i; k++) {
+            printf("   "); 
+        }
+
+        for (j = 1; j <= i; j++) {
+            printf("%3d ", num); 
+            num++;
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
