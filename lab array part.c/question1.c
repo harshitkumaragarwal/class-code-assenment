4c926a9078984bb3a76080455c5aaac91a4b0fea
harshitkumaragarwal <agarwalharshit063@gmail.com>
2025-09-30 10:20:10 +0530

    largest number find by aary

---
+++ lab array part.c/question1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+
+    int i,n;
+
+    printf("enter the number n");
+    scanf("%d",&n);
+
+    int arr[n];
+
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    int largest=arr[0];
+
+    for(i=1;i<n;i++){
+        if(arr[i]>largest){
+            largest=arr[i];
+        }
+    }
+    printf("largest number %d",largest);
+    return 0;
+}
