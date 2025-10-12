532f31973ef665b68e9ede27d15c36fcb78f721d
harshitkumaragarwal <agarwalharshit063@gmail.com>
2025-10-12 14:51:43 +0530

    reverse two number and the add teo number in reversed form

---
+++ leet code question practice.c/reversenumber.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n1, n2;
+    int reversed1 = 0, reversed2 = 0;
+    int remainder1, remainder2;
+    int sum;
+
+    // Read input
+    printf("Enter two numbers: ");
+    scanf("%d %d", &n1, &n2);
+
+    // Reverse first number
+    while (n1 != 0) {
+        remainder1 = n1 % 10;
+        reversed1 = reversed1 * 10 + remainder1;
+        n1 = n1 / 10;
+    }
+
+    // Reverse second number
+    while (n2 != 0) {
+        remainder2 = n2 % 10;
+        reversed2 = reversed2 * 10 + remainder2;
+        n2 = n2 / 10;
+    }
+
+    sum = reversed1 + reversed2;
+
+    printf("Reversed number 1: %d\n", reversed1);
+    printf("Reversed number 2: %d\n", reversed2);
+    printf("Sum of reversed numbers: %d\n", sum);
+
+    return 0;
+}
