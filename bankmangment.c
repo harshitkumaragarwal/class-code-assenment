69804cdae9ec3c34a1fa8944f0a08b5c374f9414
harshitkumaragarwal <agarwalharshit063@gmail.com>
2025-10-15 23:38:23 +0530

    question4

---
+++ bankmangment.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+
+    int account
+
+    int number,choice;
+
+    switch(choice){
+        case 1:
+        printf("to add money");
+
+        break;
+
+        case 2:
+        printf("to withdram money");
+
+        break;
+
+        case 3:
+        printf("deposit money");
+
+        break;
+
+        case 4:
+        printf("view balance");
+
+        break;
+
+         default:
+                printf("Invalid choice! Try again.\n");
+    }
+
+}
