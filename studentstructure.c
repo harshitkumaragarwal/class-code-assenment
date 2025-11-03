cba553a3accd327e74141b991d731e35a9e81fac
harshitkumaragarwal <agarwalharshit063@gmail.com>
2025-11-03 23:43:58 +0530

    student strructure

---
+++ studentstructure.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// Defining the structure
+struct Student {
+    char name[50];
+    int roll;
+    float marks;
+};
+
+int main() {
+    struct Student s;
+
+    // Taking input
+    printf("Enter name: ");
+    scanf("%s", s.name);
+
+    printf("Enter roll number: ");
+    scanf("%d", &s.roll);
+
+    printf("Enter marks: ");
+    scanf("%f", &s.marks);
+
+    // Displaying output
+    printf("\n--- Student Details ---\n");
+    printf("Name: %s\n", s.name);
+    printf("Roll Number: %d\n", s.roll);
+    printf("Marks: %.2f\n", s.marks);
+
+    return 0;
+}
